bcaa4eaec49638a4e33eaf15b656f0fb83dcfd7f
Victor Akande <victhedev@gmail.com>
2024-02-11 16:26:48 +0100

    updated function name wiki -> summary, created search function

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,8 +1,14 @@
 import wikipedia
 
 
-def wiki(name="Jesus Christ", length=1):
+def summary(name="Jesus Christ", length=1):
     """Wikipedia Fetcher"""
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search(name):
+    """Search Wikipedia Pages"""
+
+    results = wikipedia.search(name)
+    return results
--- test_logic.py
+++ test_logic.py
@@ -1,4 +1,4 @@
 from mylib.logic import wiki
 
-def test_wiki():
+def test_summary():
     assert "Jesus" in wiki()
