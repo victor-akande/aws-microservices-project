b4dbd9c63814d61d159d01afd3ea51f5cc1ee867
Victor Akande <victhedev@gmail.com>
2024-02-12 03:04:17 +0100

    implemented web service for search and summary using fastapi & uvicorn

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-from mylib.logic import summary
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import summary as summary_logic
+from mylib.logic import search as search_logic
 
-print(summary())
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /summary or /search"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in Wikipedia"""
+
+    result = search_logic(value)
+    return {"results": result}
+
+@app.get("/summary/{value}")
+async def summary(value: str):
+    """Retreive the summary of a Wikipedia page"""
+
+    result = summary_logic(value)
+    return {"result": result}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
