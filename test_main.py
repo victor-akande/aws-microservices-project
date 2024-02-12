c14f667ca53e56d48aff4bbbaf05a50ad045e0c1
Victor Akande <victhedev@gmail.com>
2024-02-12 05:40:16 +0100

    added test for fastapi

---
+++ test_main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Wikipedia API. Call /summary or /search or /phrases"
+    }
+
+
+def test_read_phrase():
+    response = client.get("/phrases/Jesus%20Christ")
+    assert response.status_code == 200
+    assert response.json() == {
+        "result": [
+            "jesus",
+            "bc",
+            "ad",
+            "jesus christ",
+            "jesus",
+            "nazareth",
+            "jewish preacher",
+            "religious leader",
+        ]
+    }
